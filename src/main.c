4be950c4c0965cf7889740f0356dc1a37b18e432
김선준 <kseonjun@yonsei.ac.kr>
2020-09-09 15:04:17 +0000

    2020-09-10 00:04

--- src/main.c
+++ src/main.c
@@ -1,108 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define INF 100000000
+#define max(a,b) (a>b?a:b)
 
-#define STACK_MAX_SIZE 256
-#define push_stack(data) stack[stack_size++] = data
-#define pop_stack(data) data = stack[--stack_size]
-#define is_empty() (stack_size == 0)
-
-typedef struct DATA {
-	int memory, cost;
-}DATA;
-
-typedef struct node * node_ptr; 
-typedef struct node {
-	int memory, cost;
-	node_ptr next;
-}node;
-
-node_ptr root;
-
-DATA stack[STACK_MAX_SIZE];
-int stack_size;
-
-int N, M;
-int c[100];
-int m[100];
-
-void connect(node_ptr prev, node_ptr new, node_ptr curr) {
-	if(prev == NULL)
-		root = new;
-	else
-		prev->next = new;
-	new->next = curr;
-}
-
-void new_node(int m, int c, node_ptr prev, node_ptr curr) {
-	node_ptr new = (node_ptr)malloc(sizeof(node));
-	new->memory = m; new->cost = c;
-	connect(prev, new, curr);
-}
-
-void push(int m, int c) {
-	node_ptr prev = NULL;
-	node_ptr curr = NULL;
-	node_ptr tmp = NULL;
+int main(void) {
+	int n, i;
+	int input[10001];
+	int dp[10001];
 	
-	if(root == NULL) {
-		new_node(m, c, NULL, NULL);
-		return;
+	scanf("%d", &n);
+	for(i = 1; i <= n; i++)
+		scanf("%d", &input[i]);
+	
+	dp[0] = 0;
+	dp[1] = input[1];
+	dp[2] = input[1] + input[2];
+	dp[3] = max(input[1], input[2]) + input[3];
+	
+	for(i = 3; i <= n; i++) {
+		dp[i] = max(max(dp[i-2], dp[i-3]+input[i-1]), dp[i-4]+input[i-1]) + input[i];
 	}
 	
-	if(c > INF) return;
-	
-	for(curr = root; curr != NULL && curr->memory < m; prev = curr, curr = curr->next) {
-		if(curr->cost >= c) {
-			curr->memory = m;
-			curr->cost = c;
-			prev = curr;
-			curr = curr->next;
-			while(curr != NULL && curr->memory <= m) {
-				tmp = curr;
-				curr = curr->next;
-				free(tmp);
-			}
-			prev->next = curr;
-			return;
-		}
-	}
-	if(curr == NULL)
-		new_node(m, c, prev, NULL);
-	else if(curr->cost > c) {
-		if(curr->memory == m)
-			curr->cost = c;
-		else if(curr->memory > m)
-			new_node(m, c, prev, curr);
-	}
-}
-
-int main() {
-	int i, j, k;
-	DATA tmp;
-	node_ptr curr;
-	
-	scanf("%d %d", &N, &M);
-	for(i = 0; i < N; i++) 
-		scanf("%d", &m[i]);
-	for(i = 0; i < N; i++) 
-		scanf("%d", &c[i]);
-	
-	push(0, 0);
-	push(60, INF);
-	for(i = 0; i < N; i++) {
-		for(curr = root; curr != NULL; curr = curr->next) {
-			tmp.memory = curr->memory + m[i];
-			tmp.cost = curr->cost + c[i];
-			push_stack(tmp);
-		}
-		while(!is_empty()) {
-			pop_stack(tmp);
-			push(tmp.memory, tmp.cost);
-		}
-	}
-	for(curr = root; curr != NULL; curr = curr->next) 
-		if(curr->memory >= M) break;
-	printf("%d", curr->cost);
+	printf("%d", max(dp[n-1],dp[n]));
 }
