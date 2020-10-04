0defd0985148e7f12c313c250a719e43e2336b97
Neurumaru <kseonjun@yonsei.ac.kr>
2020-10-04 23:46:20 +0900

    2020-10-04 23:46

--- clear/p1006.c
+++ clear/p1006.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
 
 #define MAX (10001)
+#define INIT(S,D) \
+D[0][0] = S[N][0]; D[0][1] = S[N][1]; \
+for(j = 1; j <= N; j++) { \
+	D[j][0] = S[j][0]; \
+	D[j][1] = S[j][1]; \
+}
 #define COVER(a,b,c,d) D[a][b]+D[c][d]<=W
 
-int getmin(int D[MAX][2], int N, int W) {
+int getmin(int ** D, int N, int W) {
 	int a[MAX], b[MAX], c[MAX], i;
 	
 	b[0] = c[0] = 1;
-	a[0] = COVER(0,0,0,1)?1:2;
+	a[0] = D[0][0] + D[0][1] <= W?1:2;
 	c[1] = a[0] + 1;
+	if(D[0][0] + D[1][0] <= W && D[0][1] + D[1][1] <= W)
+		a[1] = 2;
 	
 	for(i = 1; i <= N; i++) {
 		c[i] = a[i-1] + 1;
-		if(COVER(i-1, 0, i, 0) && c[i] > b[i-1] + 1)
+		if(D[i-1][0] + D[i][0] <= W && c[i] > b[i-1] + 1)
 			c[i] = b[i-1] + 1;
-		
+	
 		b[i] = a[i-1] + 1;
-		if(COVER(i-1, 1, i, 1) && b[i] > c[i-1] + 1)
+		if(D[i-1][1] + D[i][1] <= W && b[i] > c[i-1] + 1)
 			b[i] = c[i-1] + 1;
-		
+	
 		a[i] = (b[i]>c[i]?c[i]:b[i]) + 1;
-		if(COVER(i-1, 0, i, 0) && COVER(i-1, 1, i, 1) &&
+		if(D[i-1][0] + D[i][0] <= W && D[i-1][1] + D[i][1] <= W &&
 			i > 1 && a[i] > a[i-2] + 2)
 			a[i] = a[i-2] + 2;
-		else if(i == 1 && COVER(0,0,1,0) && COVER(0,1,1,1))
-			a[1] = 2;
-		if(COVER(i, 0, i, 1) && a[i] > a[i-1] + 1)
+		if(D[i][0] + D[i][1] <= W && a[i] > a[i-1] + 1)
 			a[i] = a[i-1] + 1;
-			
-		
 	}
 	
 	return a[N]-2;
@@ -36,7 +40,7 @@ int getmin(int D[MAX][2], int N, int W) {
 
 int main(void) {
 	int T, N, W, i, j, k, n;
-	int E[MAX][2];
+	int E[MAX][2], D[MAX][2];
 	
 	scanf("%d", &T);
 	for(i = 0; i < T; i++) {
@@ -44,33 +48,30 @@ int main(void) {
 		for(j = 0; j < N; j++)
 			scanf("%d", &E[j][0]);
 		for(j = 0; j < N; j++)
-			scanf("%d", &E[j][1]);
+			scanf("%d", &E[j][0]);
 		
-		E[N][0] = E[0][0];
-		E[N][1] = E[0][1];
-		E[0][0] = E[0][1] = W;
-		n = getmin(E, N, W);
+		INIT(D, E);
+		D[0][0] = D[0][1] = W;
+		n = getmin(D, N, W);
 		
-		E[0][0] = E[N][0];
-		E[0][1] = E[N][1];
-		E[N][0] = E[0][1] = W;
-		j = getmin(E, N, W);
+		INIT(D, E);
+		D[N][0] = D[0][1] = W;
+		j = getmin(D, N, W);
 		if(n > j) n = j;
 		
-		E[N][0] = E[0][0];
-		E[0][1] = E[N][1];
-		E[0][0] = E[N][1] = W;
-		j = getmin(E, N, W);
+		INIT(D, E);
+		D[0][0] = D[N][1] = W;
+		j = getmin(D, N, W);
 		if(n > j) n = j;
 		
-		E[0][0] = E[N][0];
-		E[N][1] = E[0][1];
-		E[N][0] = E[N][1] = W;
-		j = getmin(E, N, W);
+		INIT(D, E);
+		D[N][0] = D[N][1] = W;
+		j = getmin(D, N, W);
 		if(n > j) n = j;
 		
-		printf("%d\n", n);
+		printf("%d", n);
 	}
 	
 	return 0;
 }
+    
