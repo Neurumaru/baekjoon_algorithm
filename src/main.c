4288801cc6ae8a8d6390a489917887b85bc67612
Neurumaru <kseonjun@yonsei.ac.kr>
2020-10-06 00:50:46 +0900

    2020-10-06 00:50

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define QUEUE_MAX_SIZE 2500
+#define QUEUE_MAX_SIZE 250000
 #define push(data) queue[head++] = data; head %= QUEUE_MAX_SIZE
 #define pop(data) data = queue[tail++]; tail %= QUEUE_MAX_SIZE
 #define is_empty() (head == tail)
@@ -13,58 +13,64 @@ DATA queue[QUEUE_MAX_SIZE];
 int head, tail;
 
 int main(void) {
-	int T, M, N, K, X, Y, i, j, k, result, ax, ay;
-	int map[50][50];
+	int M, N, i, j, k, ax, ay, result;
+	int map[1000][1000];
 	int dx[] = {0, 0, -1, 1};
 	int dy[] = {-1, 1, 0, 0};
 
 	DATA data, tmp;
 
+	scanf("%d %d", &M, &N);
 
-	scanf("%d", &T);
-	while(T--) {
-		scanf("%d %d %d", &M, &N, &K);
-	
-		result = 0;	
-		for(i = 0; i < N; i++) {
-			for(j = 0; j < M; j++) {
-				map[i][j] = 0;
-			}
+	for(i = 0; i < N; i++) {
+		for(j = 0; j < M; j++) {
+			scanf("%d", &map[i][j]);
 		}
+	}
 
-		for(i = 0; i < K; i++) {
-			scanf("%d %d", &X, &Y);
+	for(i = 0; i < N; i++) {
+		for(j = 0; j < M; j++) {
+			if(map[i][j] == 1) {
+				tmp.x = i; tmp.y = j;
+				push(tmp);
+				while(!is_empty()) {
+					pop(data);
+					for(k = 0; k < 4; k++) {
+						ax = data.x + dx[k]; ay = data.y + dy[k];
+						
+						if(ax < 0 || ax >= N || ay < 0 || ay >= M)
+							continue;
 
-			map[Y][X] = 1;
-		}
+						if(map[ax][ay] == -1)
+							continue;
 
-		for(i = 0; i < N; i++) {
-			for(j = 0; j < M; j++) {
-				if(map[i][j] == 1) {
-					map[i][j] = 0;
-					result++;
-					tmp.y = i; tmp.x = j;
-					push(tmp);
-					while(!is_empty()) {
-						pop(data);
-						for(k = 0; k < 4; k++) {
-							ay = data.y + dy[k]; ax = data.x + dx[k];
-							if(0 > ay || ay >= N || 0 > ax || ax >= M)
-								continue;
+						if(map[ax][ay] > map[data.x][data.y]+1 || map[ax][ay] == 0) {
+							map[ax][ay] = map[data.x][data.y]+1;
 
-							if(map[ay][ax] == 1) {
-								map[ay][ax] = 0;
-								tmp.y = ay; tmp.x = ax;
-								push(tmp);
-							}
+							tmp.x = ax; tmp.y = ay;
+							push(tmp);
 						}
 					}
 				}
 			}
 		}
-
-		printf("%d\n", result);
 	}
+
+	result = 0;
+	for(i = 0; i < N; i++) {
+		for(j = 0; j < M; j++) {
+			if(map[i][j] == 0) {
+				printf("-1\n");
+				return 0;
+			}
+			
+			if(map[i][j] > result) {
+				result = map[i][j];
+			}
+		}
+	}
+
+	printf("%d\n", result-1);
 
 	return 0;
 }
