f7c75c20448b52797af9aa97583b2d6d7ac60b14
Neurumaru <kseonjun@yonsei.ac.kr>
2020-10-11 19:23:02 +0900

    2020-10-11 19:23

--- src/main.c
+++ src/main.c
@@ -2,24 +2,44 @@
 
 #include <stdio.h>
 
+#define INF 1000000
 #define min(a,b) ((a)>(b)?(b):(a))
 
+#define QUEUE_MAX_SIZE 100001
+#define push(data) queue[head++] = data; head %= QUEUE_MAX_SIZE
+#define pop(data) data = queue[tail++]; tail %= QUEUE_MAX_SIZE
+#define is_empty() (head == tail)
+
+int queue[QUEUE_MAX_SIZE];
+int head, tail;
+
 int main(void) {
-	int N, K;
-	int i, j, k;
+	int N, K, i;
 	int dp[100001];
 
-	scanf("%d %d", &N, &K);
-	
-	for(i = 0; i < N; i++)
-		dp[i] = N - i;
-	dp[N] = 0;
-	for(i = N+1; i <= K; i++) {
-		if(i%2 == 0)
-			dp[i] = min(dp[i/2], dp[i-1]) + 1;
-		else
-			dp[i] = dp[i-1] + 1;
+	for(i = 0; i <= 100000; i++) {
+		dp[i] = INF;
 	}
-	
+
+	scanf("%d %d", &N, &K);
+
+	push(N);
+	dp[N] = 0;
+	while(!is_empty()) {
+		pop(i);
+		if(i-1 >= 0 && dp[i-1] == INF) {
+			dp[i-1] = dp[i] + 1;
+			push(i-1);
+		}
+		if(i+1 <= 100000 && dp[i+1] == INF) {
+			dp[i+1] = dp[i] + 1;
+			push(i+1);
+		}
+		if(i*2 <= 100000 && dp[i*2] == INF) {
+			dp[i*2] = dp[i] + 1;
+			push(i*2);
+		}
+	}
+
 	printf("%d\n", dp[K]);
 }
